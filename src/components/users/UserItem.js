4e0cc1d782389e2dfd226667b494015d9f95ec8b
Dima <dreykhtman@gmail.com>
2020-04-13 15:11:47 -0400

    finished section 3.12

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -4,14 +4,14 @@ import React, { Component } from 'react';
 
 class UserItem extends Component {
   state = {
-    id: 'id',
+    id: '1',
     login: 'mojombo',
     avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
     html_url: 'https://github.com/mojombo',
   };
 
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img
@@ -22,7 +22,9 @@ class UserItem extends Component {
         />
         <h3>{login}</h3>
         <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1"></a>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            View
+          </a>
         </div>
       </div>
     );
