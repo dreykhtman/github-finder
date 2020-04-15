0edfe24d540dca216f1e0bfe9ef57e8886a1d02b
Dima <dreykhtman@gmail.com>
2020-04-14 22:02:58 -0400

    finished section 3.17

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends Component {
@@ -15,10 +16,6 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    console.log(
-      process.env.REACT_APP_GITHUB_CLIENT_SECRET,
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-    );
     this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
@@ -32,6 +29,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users users={users} loading={loading} />
         </div>
       </div>
