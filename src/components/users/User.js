0d200dd1b14c5c47e699ff5ace6ab8de95b1d243
Dima <dreykhtman@gmail.com>
2020-04-16 16:38:25 -0400

    finished section 3.23

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react';
+/* eslint-disable camelcase */
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 
 export class User extends Component {
   componentDidMount() {
@@ -8,6 +12,8 @@ export class User extends Component {
   render() {
     const {
       name,
+      company,
+      hireable,
       avatar_url,
       location,
       bio,
@@ -17,13 +23,93 @@ export class User extends Component {
       followers,
       following,
       public_repos,
-      public_gists_hireable,
+      public_gists,
     } = this.props.user;
 
     const { loading } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back To Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt=""
+              className="round-img"
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a
+              href={html_url}
+              className="btn btn-dark my-1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit GitHub Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>{' '}
+                    <a href={blog} target="_blank" rel="noopener noreferrer">
+                      {blog}
+                    </a>
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
+
+User.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+};
 
 export default User;
